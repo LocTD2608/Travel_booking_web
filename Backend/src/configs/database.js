7f6aa46f8d0bab23ca0326f2fbb860de34cacae1
Hoang <Hoangtv.ptit@gmail.com>
2026-04-09 21:58:32 +0700

    fix loi api

--- Backend/src/configs/database.js
+++ Backend/src/configs/database.js
@@ -1,15 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize("quanly_booking", "root", "280106", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 module.exports = sequelize;
